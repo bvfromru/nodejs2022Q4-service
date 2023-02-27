fb7cc9d77a153c092394b18380b070566a233f66
Vitaliy Budkin <bvfromru@gmail.com>
2023-02-27 22:08:02 +0400

    feat: implement route logger middleware

--- src/common/logger/logger.service.ts
+++ src/common/logger/logger.service.ts
@@ -8,38 +8,27 @@ export class CustomLogger implements LoggerService {
     this.logLevel = configService.get<number>('LOGGER_LEVEL');
   }
 
-  /**
-   * Write a 'log' level log.
-   */
   log(message: any, ...optionalParams: any[]) {
-    console.log(message);
+    console.log('\x1b[32m', message, '\x1b[37m');
   }
 
-  /**
-   * Write an 'error' level log.
-   */
   error(message: any, ...optionalParams: any[]) {
-    console.log(message);
+    console.log('\x1b[31m', message, '\x1b[37m');
   }
 
-  /**
-   * Write a 'warn' level log.
-   */
   warn(message: any, ...optionalParams: any[]) {
-    console.log(message);
+    console.log('\x1b[33m', message, '\x1b[37m');
   }
 
-  /**
-   * Write a 'debug' level log.
-   */
   debug?(message: any, ...optionalParams: any[]) {
-    console.log(message);
+    console.log(message, '\x1b[37m');
   }
 
-  /**
-   * Write a 'verbose' level log.
-   */
   verbose?(message: any, ...optionalParams: any[]) {
-    console.log(message);
+    console.log('\x1b[34m', message, '\x1b[37m');
   }
+
+  // write(message: any) {
+  //   console.log(message, type);
+  // }
 }
