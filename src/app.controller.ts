768e7082d1fc739030f1592d93586e2d48fed7c4
Vitalii Budkin <bvfromru@gmail.com>
2023-02-27 14:40:11 +0400

    fix: fix docs route and root route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { Public } from './utils/constants';
+@Public()
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
 }
